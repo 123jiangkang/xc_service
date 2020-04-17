ef872160b2ba796bcd03d364367b130634d844cd
root <1021600405@qq.com>
2020-04-18 00:32:56 +0800

    001

--- xc-framework-parent/runPost.sh
+++ xc-framework-parent/runPost.sh
@@ -3,7 +3,7 @@
 image=`sudo docker images | grep -w $1 | head -n 1 | awk '{print $3}'`
 
 #启动容器
-sudo docker run -itd --name $1 -p 31002:31002 $image
+sudo docker run -itd --name $1 -p 31002:31001 $image
 
 if [ $? -eq 0 ];then
       echo "$1 容器已启动"
